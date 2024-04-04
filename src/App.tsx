dc4954de84f19f806c88c0eadb343a5a15730071
Dmytro Pedan <dmitrijpedan84@gmail.com>
2024-04-04 10:22:15 +0200

    feature: configure tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <h1 className="text-xl text-red-500">App</h1>
       <p>Added pre-commit with husky</p>
     </div>
   );
