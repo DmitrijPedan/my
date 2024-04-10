c8b4a73fd719c079d17f3a3f0449859652a5acba
Dmytro Pedan <dmitrijpedan84@gmail.com>
2024-04-10 08:12:37 +0200

    feature: added background stripes for dark mode

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -21,6 +21,38 @@ const MY_EXPERIENCE: JobCardProps["job"][] = [
       </ul>
     ),
   },
+  {
+    from: "2019",
+    to: "present",
+    company: "Codevery LLC",
+    companyHref: "https://codevery.com",
+    position: "Frontend Engineer",
+    responsibilities: (
+      <ul>
+        <li>Developing and maintaining websites and web applications</li>
+        <li>Collaborating with designers to create responsive and interactive web designs</li>
+        <li>Optimizing web applications for maximum speed and scalability</li>
+        <li>Writing clean, maintainable, and efficient code</li>
+        <li>Participating in code reviews and team meetings</li>
+      </ul>
+    ),
+  },
+  {
+    from: "2019",
+    to: "present",
+    company: "Codevery LLC",
+    companyHref: "https://codevery.com",
+    position: "Frontend Engineer",
+    responsibilities: (
+      <ul>
+        <li>Developing and maintaining websites and web applications</li>
+        <li>Collaborating with designers to create responsive and interactive web designs</li>
+        <li>Optimizing web applications for maximum speed and scalability</li>
+        <li>Writing clean, maintainable, and efficient code</li>
+        <li>Participating in code reviews and team meetings</li>
+      </ul>
+    ),
+  },
 ];
 
 function AppContent() {
--- src/layout/Page.tsx
+++ src/layout/Page.tsx
@@ -3,9 +3,11 @@ import Header from "src/components/Header";
 
 function Page({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="relative flex min-h-screen flex-col">
+      <div className="fixed left-0 top-0 h-[200vh] w-[15vw] -translate-y-1/2 translate-x-1/3 rotate-45 bg-gradient-to-r opacity-60 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900" />
+      <div className="fixed bottom-0 right-0 h-[200vh] w-[20vw] -translate-x-3/4 translate-y-1/2 rotate-45 bg-gradient-to-r opacity-60 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900" />
       <Header />
-      <main className="container mx-auto max-w-6xl px-4 md:px-6">{children}</main>
+      <main className="container relative z-10 mx-auto max-w-6xl px-4 md:px-6">{children}</main>
     </div>
   );
 }
