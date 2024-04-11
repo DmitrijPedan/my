82778a8688dd863a3df568384c7c8a284367c7d6
Dmytro Pedan <dmitrijpedan84@gmail.com>
2024-04-11 23:40:14 +0200

    feature: added content to 404 page

--- src/layout/Page.tsx
+++ src/layout/Page.tsx
@@ -1,13 +1,19 @@
+import classNames from "classnames";
 import React from "react";
+import Footer from "src/components/Footer";
 import Header from "src/components/Header";
 
-export function Page({ children }: { children: React.ReactNode }) {
+export function Page({ children, className }: { children: React.ReactNode; className?: string }) {
   return (
     <div className="relative flex min-h-screen flex-col">
       <div className="fixed left-0 top-0 h-[200vh] w-[15vw] -translate-y-1/2 translate-x-1/3 rotate-45 bg-gradient-to-r opacity-60 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900" />
       <div className="fixed bottom-0 right-0 h-[200vh] w-[20vw] -translate-x-3/4 translate-y-1/2 rotate-45 bg-gradient-to-r opacity-60 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900" />
       <Header />
-      <main className="container relative z-10 mx-auto max-w-6xl px-4 md:px-6">{children}</main>
+      <main
+        className={classNames("container relative z-10 mx-auto max-w-6xl flex-auto px-4 md:px-6", className)}>
+        {children}
+      </main>
+      <Footer />
     </div>
   );
 }
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,9 +1,24 @@
+import { ArrowLongLeftIcon } from "@heroicons/react/24/solid";
+import { NavLink } from "react-router-dom";
 import { Page } from "src/layout";
 
 export function NotFoundPage() {
   return (
-    <Page>
-      <h1>Not Found</h1>
+    <Page className="flex flex-col items-center justify-center">
+      <h1 className="mb-8  max-w-xl text-center text-2xl leading-normal sm:text-3xl lg:text-4xl">
+        Oops! <br /> You&apos;ve Discovered Uncharted Territory...
+      </h1>
+      <p className="mb-14 max-w-2xl text-center text-base leading-relaxed sm:text-lg lg:text-xl">
+        Hey there, adventurer! It looks like you&apos;ve taken a turn into a digital realm that even we
+        haven&apos;t explored yet. Don&apos;t worry, though – no web wanderer gets left behind. Let&apos;s
+        navigate you back to familiar lands.
+      </p>
+      <NavLink
+        to="/"
+        className="group mt-8 flex items-center text-lg text-blue-500 transition-colors hover:text-emerald-500 dark:text-white">
+        <ArrowLongLeftIcon className="mr-2 inline-block h-5 w-5 transition-transform group-hover:-translate-x-1" />
+        <span>Home</span>
+      </NavLink>
     </Page>
   );
 }
