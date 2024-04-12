dd906a0e6f0c90e2bf3f6534ec6a0f0308c7327c
Dmytro Pedan <dmitrijpedan84@gmail.com>
2024-04-12 19:49:13 +0200

    feature: added index.html to tailwind watcher

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "selector",
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
       fontFamily: {
