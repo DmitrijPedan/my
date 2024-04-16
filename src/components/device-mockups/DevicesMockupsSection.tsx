03f035295b6d3130685e776aefdca96c4f267bed
Dmytro Pedan <dmitrijpedan84@gmail.com>
2024-04-16 09:52:24 +0200

    feature: added preloading state to the mockups section

--- src/components/device-mockups/DevicesMockupsSection.tsx
+++ src/components/device-mockups/DevicesMockupsSection.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { useEffect, useState } from "react";
 import { DesktopMockup, MobileMockup, TabletMockup } from "src/components/device-mockups/DevicesMockups";
 import type { Project } from "src/constants/projects";
 
@@ -9,10 +10,29 @@ function DevicesMockupsSection({
   detailedInfo: Project["detailedInfo"];
   className?: string;
 }) {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!detailedInfo?.devices) return;
+    const images = [detailedInfo.devices.mobile, detailedInfo.devices.tablet, detailedInfo.devices.desktop];
+    const promises = images.map((src) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = resolve;
+      });
+    });
+
+    Promise.all(promises).then(() => {
+      setLoaded(true);
+    });
+  }, []);
+
   return detailedInfo?.devices ? (
     <div
       className={classNames(
         "relative mx-auto flex max-w-6xl flex-col flex-wrap items-center justify-center sm:flex-row md:justify-between",
+        !loaded ? "h-0 opacity-0" : "animate-fade-in-sm",
         className
       )}>
       <div className="relative z-10 order-1 max-w-full p-2 sm:p-4 md:order-2 md:-mt-96 lg:ml-20">
