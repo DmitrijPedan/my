90a4821dc368591e37fa2b0110dd258f438e03b8
Dmytro Pedan <dmitrijpedan84@gmail.com>
2024-04-21 14:54:27 +0200

    feature: rearranged footer items

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import React from "react";
 import Link from "src/components/Link";
+import { GithubIcon } from "src/components/icons";
 
 function Footer({ className, ...rest }: React.HTMLAttributes<HTMLDivElement>) {
   return (
@@ -8,34 +9,44 @@ function Footer({ className, ...rest }: React.HTMLAttributes<HTMLDivElement>) {
       className={classNames("mx-auto flex w-full max-w-7xl flex-col items-center px-2", className)}
       {...rest}>
       <hr className="border-top w-full border-slate-300 dark:border-slate-700" />
-      <div className="flex w-full flex-row items-center justify-between py-3 text-slate-700 dark:text-slate-400">
-        <p className="mb-2 mr-3 w-5/12 text-sm md:mb-0 md:w-1/3">© 2019 - {new Date().getFullYear()}.</p>
-        <p className="w-6/12 text-left text-sm md:w-2/3 md:text-right">
-          Developed using{" "}
+      <div className="flex w-full flex-col items-center justify-center py-3 text-slate-700 md:flex-row md:justify-between dark:text-slate-400">
+        <div className="flex">
           <Link
-            aria-label="Open Visual Studio Code site in a new tab"
-            href="https://code.visualstudio.com/"
+            aria-label="See this project on GitHub"
+            href="https://github.com/DmitrijPedan/DmitrijPedan.github.io"
             target="_blank"
-            rel="noopener noreferrer">
-            Visual Studio Code
+            rel="noopener noreferrer"
+            className="mr-2 inline-block fill-white text-white">
+            <GithubIcon className="h-6 w-6 md:h-5 md:w-4" />
           </Link>
-          , powered by{" "}
-          <Link
-            aria-label="Open React site in a new tab"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer">
-            React
-          </Link>
-          , and{" "}
-          <Link
-            aria-label="Open Tailwind CSS site in a new tab"
-            href="https://tailwindcss.com/"
-            target="_blank"
-            rel="noopener noreferrer">
-            Tailwind CSS
-          </Link>
-        </p>
+          <p className="text-center text-sm">
+            <span>Developed using </span>
+            <Link
+              aria-label="Open Visual Studio Code site in a new tab"
+              href="https://code.visualstudio.com/"
+              target="_blank"
+              rel="noopener noreferrer">
+              Visual Studio Code
+            </Link>
+            <span>, powered by </span>
+            <Link
+              aria-label="Open React site in a new tab"
+              href="https://reactjs.org/"
+              target="_blank"
+              rel="noopener noreferrer">
+              React
+            </Link>
+            <span>, and </span>
+            <Link
+              aria-label="Open Tailwind CSS site in a new tab"
+              href="https://tailwindcss.com/"
+              target="_blank"
+              rel="noopener noreferrer">
+              Tailwind CSS
+            </Link>
+          </p>
+        </div>
+        <p className="ml-3 mt-2 text-sm md:mt-0">© 2019 - {new Date().getFullYear()}.</p>
       </div>
     </footer>
   );
--- src/components/buttons/OnTopButton.tsx
+++ src/components/buttons/OnTopButton.tsx
@@ -40,7 +40,7 @@ export function OnTopButton({ className, ...rest }: React.HTMLAttributes<HTMLBut
       data-twe-ripple-init
       data-twe-ripple-color="light"
       className={classNames(
-        "z-100 !fixed bottom-16 left-5 hidden cursor-pointer rounded-full p-3 text-white shadow-md transition duration-150 ease-in-out focus:ring-0 md:bottom-5 md:left-[initial] md:right-5",
+        "z-100 !fixed bottom-24 right-5 hidden cursor-pointer rounded-full p-3 text-white shadow-md transition duration-150 ease-in-out focus:ring-0 md:bottom-10 md:left-[initial] md:right-5",
         "bg-slate-700 hover:bg-sky-500 focus:bg-sky-500 active:bg-sky-500",
         "dark:bg-slate-800 dark:text-sky-500 dark:hover:bg-sky-600 dark:hover:text-white dark:focus:bg-sky-600 dark:focus:text-white dark:active:bg-sky-600 dark:active:text-white",
         className
